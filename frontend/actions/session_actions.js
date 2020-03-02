14933e94a91d0b768222200f39aa4e31cf6c91ee
Sushil <sushilt553@gmail.com>
2020-03-02 14:34:44 -0500

    Session actions creator/thunk actions creator created

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,32 @@
+import * as SessionApiUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+
+export const receiveCurrentUser = (user) => {
+    return {
+        type: RECEIVE_CURRENT_USER,
+        user
+    }
+}
+
+export const logoutCurrentUser = () => {
+    return {
+        type: LOGOUT_CURRENT_USER
+    }
+}
+
+export const createUser = (user) => dispatch => (
+    SessionApiUtil.signup(user)
+    .then(user => dispatch(receiveCurrentUser(user)))
+)
+
+export const loginUser = (user) => dispatch => (
+    SessionApiUtil.login(user)
+    .then(user => dispatch(receiveCurrentUser(user)))
+)
+
+export const logout = () => dispatch => (
+    SessionApiUtil.logout()
+    .then(() => dispatch(logoutCurrentUser()))
+)
