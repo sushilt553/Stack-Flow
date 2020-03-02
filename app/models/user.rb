779ba27cbc3e92ab833c8f4a74aeea34c8b0cb68
Sushil <sushilt553@gmail.com>
2020-03-02 13:33:51 -0500

    User model/back end auth created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,38 @@
 class User < ApplicationRecord
+
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, length: {minimum: 6, allow_nil: true}
+
+    attr_reader :password
+    after_initialize :ensure_session_token
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return user if user && user.is_password?(password)
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64(16)
+    end
+
+    def reset_session_token!
+        self.update!(session_token: self.class.generate_session_token)
+        self.session_token
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        bcrypted = BCrypt::Password.new(self.password_digest)
+        bcrypted.is_password?(password)
+    end
+
+    private
+    def ensure_session_token
+        self.session_token ||= self.class.generate_session_token
+    end
 end
