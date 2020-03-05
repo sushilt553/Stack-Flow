4704845085abde4e927f60a2fa54a68adf125bf6
Sushil <sushilt553@gmail.com>
2020-03-05 14:21:34 -0500

    Sidebar created

--- frontend/components/main/home_page.jsx
+++ frontend/components/main/home_page.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import Sidebar from '../main/sidebar';
 import QuestionIndexContainer from '../questions/questions_index_container';
 
 class HomePage extends React.Component{
 
     render(){
         return (
-            <div>
-                <button><Link to='/questions/new'>Ask Question</Link></button>
-                <QuestionIndexContainer />
+            <div className="main-home-page">
+                <div className="side-bar"><Sidebar /></div>
+                <section className="index-container">
+                    <button><Link to='/questions/new'>Ask Question</Link></button>
+                    <div className="question-index"><QuestionIndexContainer /></div>
+                </section>
             </div>
         )
     }
---
+++ frontend/components/main/sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <div className="div-sidebar">
+            <p>Home</p>
+            <p>PUBLIC</p>
+            <ul className="tag-lists">
+                <li>Tags</li>
+                <li>Users</li>
+                <li>Jobs</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Sidebar;
