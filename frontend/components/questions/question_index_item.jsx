4d5cff7d81f24255ca46fa00a7e60c6e99d8a6a1
Sushil <sushilt553@gmail.com>
2020-03-06 12:33:16 -0500

    Tags added in the main page

--- frontend/components/questions/question_index_item.jsx
+++ frontend/components/questions/question_index_item.jsx
@@ -2,20 +2,17 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const QuestionIndexItem = (props) => {
-    
-//    const buttons = (props.question.author_id === props.sessionId) ?
-//         <>
-//             <button onClick={() => props.deleteQuestion(props.question.id)}>Delete</button>
-//             <button><Link to={`/questions/${props.question.id}/edit`}>Edit</Link></button>
-//         </>
-//         :
-//         null;
-    
+    const tagsArr = props.question.tags.split(" ")
+    const tags = tagsArr.map((tag, idx) => <li className="tag-list" key={idx} >{tag}</li>);
     return (
         <li className="single-question" key={props.question.id}>
             <Link className="question-body" to={`/questions/${props.question.id}`} >{props.question.title}</Link>
-            {/* {buttons} */}
+            <ul className="all-show-tags">
+            <div className="show-tags">
+                {tags}
+            </div>
             <p className="posted-by">Posted by {props.question.author_name}</p>
+            </ul>
         </li>
     )
 }
