c6a72b8157c486a796ef5e0f9ec89d0675e6b8a9
Sushil <sushilt553@gmail.com>
2020-03-11 12:03:10 -0400

    Updated migration file

--- db/migrate/20200311154729_remove_null_false_for_views.rb
+++ db/migrate/20200311154729_remove_null_false_for_views.rb
@@ -1,6 +1,6 @@
 class RemoveNullFalseForViews < ActiveRecord::Migration[5.2]
   def change
-    remove_column :questions, :views
+    remove_column :questions, :views, :integer
     add_column :questions, :views, :integer
   end
 end
