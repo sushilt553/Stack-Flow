0aa06c7edc15460bd5f639e8dad2360d416f5c4f
Sushil <sushilt553@gmail.com>
2020-03-12 13:56:48 -0400

    Tag head added

--- frontend/components/tags/tags_index.jsx
+++ frontend/components/tags/tags_index.jsx
@@ -20,9 +20,13 @@ class TagsIndex extends React.Component{
         return (
             <div className="all-tags-index">
                 <Sidebar />
-                <ul className="tags-list-index">
-                    {tags}
-                </ul>
+                <div className="tags-header">
+                    <p className='tag-head'>Tags</p>
+                    <p className='tag-p'>A tag is a keyword or label that categorizes your question with other, similar questions. Using the right tags makes it easier for others to find and answer your question.</p>
+                    <ul className="tags-list-index">
+                        {tags}
+                    </ul>
+                </div>
             </div>
         ) 
     }
